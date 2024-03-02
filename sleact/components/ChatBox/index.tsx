654e7a389fe177012be9801c8222ef56ae8b2b1e
HeeJung Kang <tentasys@gmail.com>
2024-03-02 22:08:51 +0900

    챗박스 만들기

--- sleact/components/ChatBox/index.tsx
+++ sleact/components/ChatBox/index.tsx
@@ -1,18 +1,43 @@
-import React, {useCallback, VFC} from "react";
+import React, {useCallback, useEffect, useRef, VFC} from "react";
 import {ChatArea, Form, MentionsTextarea, SendButton, Toolbox} from "@components/ChatBox/styles";
+import autosize from 'autosize';
 
 interface Props {
-    chat: String;
+    chat: string;
+    onSubmitForm: (e: any) => void;
+    onChangeChat: (e: any) => void;
+    placeholder?: string;
 }
 
-const ChatBox: VFC<Props> = ({ chat }) => {
-    const onSubmitForm = useCallback(() => {}, []);
+const ChatBox: VFC<Props> = ({ chat , onSubmitForm, onChangeChat, placeholder}) => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    useEffect(() => {
+        if (textareaRef.current) {
+            autosize(textareaRef.current);
+        }
+    }, []);
+    const onKeydownChat = useCallback((e) => {
+        // console.log(e);
+        if(!e.nativeEvent.isComposing && e.key === 'Enter') {
+        // if(e.key === 'Enter') {
+            if (!e.shiftKey) {
+                e.preventDefault();
+                onSubmitForm(e);
+            }
+        }
+    }, [onSubmitForm]);
+
     return (
         <ChatArea>
             <Form onSubmit={onSubmitForm}>
-                <MentionsTextarea>
-                    <textarea />
-                </MentionsTextarea>
+                <MentionsTextarea
+                    id="editor-chat"
+                    value={chat}
+                    onChange={onChangeChat}
+                    onKeyDown={onKeydownChat}
+                    placeholder={placeholder}
+                    ref={textareaRef}
+                />
                 <Toolbox>
                     <SendButton
                         className={
--- sleact/pages/DirectMessage/index.tsx
+++ sleact/pages/DirectMessage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback} from "react";
 import {Container, Header} from "@pages/DirectMessage/styles";
 import gravatar from "gravatar";
 import useSWR from "swr";
@@ -6,11 +6,17 @@ import fetcher from "@utils/fetcher";
 import {useParams} from "react-router";
 import ChatBox from "@components/ChatBox";
 import ChatList from "@components/ChatList";
+import useInput from "@hooks/useInput";
 
 const DirectMessage = () => {
     const { workspace, id } = useParams<{ workspace: string, id: string }>();
     const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
     const { data: myData } = useSWR(`/api/users`, fetcher);
+    const [chat, onChangeChat] = useInput('');
+    const onSubmitform = useCallback((e) => {
+        e.preventDefault();
+        console.log('submit');
+    }, [])
 
     // 값이 없을 때 (로딩중일 때)는 화면 띄우지 않기
     if(!userData || !myData) {
@@ -23,7 +29,7 @@ const DirectMessage = () => {
                 <span>{userData.nickname}</span>
             </Header>
             <ChatList />
-            <ChatBox chat=""/>
+            <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitform}/>
         </Container>
     );
 }
