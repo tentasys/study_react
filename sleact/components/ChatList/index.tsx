40844e2be23efef9b4aab04eb2229855e5effe18
HeeJung Kang <tentasys@gmail.com>
2024-03-12 23:00:30 +0900

    커스텀 스크롤바, dayjs 적용

--- sleact/components/ChatList/index.tsx
+++ sleact/components/ChatList/index.tsx
@@ -1,20 +1,27 @@
-import React, {VFC} from 'react';
+import React, {useCallback, useRef, VFC} from 'react';
 import {ChatZone, Section} from "@components/ChatList/styles";
 import {IDM} from "@typings/db";
 import Chat from "@components/Chat";
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
     chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
-  return (
+    const scrollbarRef = useRef(null);
+    const onScroll = useCallback(() => {
+
+    }, [])
+   return (
       <ChatZone>
-          {
-              chatData?.map((chat) => (
-                  <Chat key={chat.id} data={chat} />
-              ))
-          }
+          <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+              {
+                  chatData?.map((chat) => (
+                      <Chat key={chat.id} data={chat} />
+                  ))
+              }
+          </Scrollbars>
       </ChatZone>
   )
 };
