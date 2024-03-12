95e1b0ab6cc236270e84f370d14afd0288d4fcf8
HeeJung Kang <tentasys@gmail.com>
2024-03-12 22:33:49 +0900

    DM 내용 표시하기

--- sleact/components/ChatList/index.tsx
+++ sleact/components/ChatList/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {VFC} from 'react';
 import {ChatZone, Section} from "@components/ChatList/styles";
+import {IDM} from "@typings/db";
+import Chat from "@components/Chat";
 
-const ChatList = () => {
+interface Props {
+    chatData?: IDM[];
+}
+
+const ChatList: VFC<Props> = ({ chatData }) => {
   return (
       <ChatZone>
-          <Section>section</Section>
+          {
+              chatData?.map((chat) => (
+                  <Chat key={chat.id} data={chat} />
+              ))
+          }
       </ChatZone>
   )
 };
